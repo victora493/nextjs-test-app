87be23603330e8671df25faa2505ea13df1bd120
Victor <71620855+victora493@users.noreply.github.com>
2021-08-15 19:10:02 -0700

    again tried to fix loadind imaeg stat

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList"
 
 function HomePage({ meetups }) {
+    const shimmer = (w, h) => `
+    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+        </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+    </svg>`
+
+    const toBase64 = (str) => {
+        return typeof window === 'undefined'
+        ? Buffer.from(str).toString('base64')
+        : window.btoa(str)
+    }
+
     return (
         <>
             <Head>
                 <title>React Meetups</title>
                 <meta name="description" content="Browse a huge list of highly active React meetups!" />
             </Head>
+
+            <Image 
+                src={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'} 
+                placeholder="blur"
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(640, 430))}`}
+                width={640} 
+                height={430}
+            />
 
             <MeetupList meetups={meetups} />
         </>
