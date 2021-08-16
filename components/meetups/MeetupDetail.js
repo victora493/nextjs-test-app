c666caa814cfb7a6ae11d3e11a7578ed7bd069ab
Victor <71620855+victora493@users.noreply.github.com>
2021-08-15 17:57:47 -0700

    optimized images

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,16 @@
 import classes from './MeetupDetail.module.css'
+import Image from 'next/image'
 
 export default function MeetupDetail({ title, image, address, description }) {
     return (
         <section className={classes.detail}>
-            <img src={image} alt="" />
+            <Image 
+                width={640} 
+                height={430} 
+                src={image} 
+                alt=""
+                priority
+            />
             <h1>{title}</h1>
             <address>{address}</address>
             <p>{description}</p>
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ['upload.wikimedia.org', '2.bp.blogspot.com', 'i.pinimg.com'],
+    },
+}
