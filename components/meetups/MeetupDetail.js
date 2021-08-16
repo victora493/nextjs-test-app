1203d9bca041a787aeddc5787e9d12e1c1a1affd
Victor <71620855+victora493@users.noreply.github.com>
2021-08-15 18:47:36 -0700

    created a correct placeholder

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,16 +3,35 @@ import Image from 'next/image'
 import dataURL from '../../util/placeHolderDataURL';
 
 export default function MeetupDetail({ title, image, address, description }) {
+    const shimmer = (w, h) => `
+    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+        </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+    </svg>`
+
+    const toBase64 = (str) =>
+    typeof window === 'undefined'
+        ? Buffer.from(str).toString('base64')
+        : window.btoa(str)
+
     return (
         <section className={classes.detail}>
             <Image 
-                width={640} 
-                height={430} 
                 src={image} 
                 alt=""
                 priority
                 placeholder="blur"
-                blurDataURL={dataURL}
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(640, 430))}`}
+                width={640} 
+                height={430} 
             />
             <h1>{title}</h1>
             <address>{address}</address>
